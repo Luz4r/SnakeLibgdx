b876d2578a87854408ab62c23d699ab9e110d37e
Luz4r <piorek.r@gmail.com>
2018-10-26 13:10:10 +0200

    Adding Snake class

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -5,127 +5,77 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.TimeUtils;
 
 
 public class GameScreen implements Screen {
 
+    private final Snake snake;
     private final Learning game;
     private OrthographicCamera camera;
-    private Texture snakeBody;
-    private Texture snakeTail;
-    private Texture snakeHead;
-    private Array<Rectangle> snakeParts;
     private boolean isMovingUp = false, isMovingRight = true, isMovingDown = false, isMovingLeft = false;
     private float rotation = 0;
-    private float rotationShared = 0;
-    private long lastDrawedFrameTime = 0;
 
     private enum SnakePart {SNAKEHEAD, SNAKETAIL, SNAKEBODY};
 
-    public GameScreen(final Learning game){
+    GameScreen(final Learning game){
         this.game = game;
 
-        snakeBody = new Texture(Gdx.files.internal("snakeBody.png"));
-        snakeTail = new Texture(Gdx.files.internal("snakeTail.png"));
-        snakeHead = new Texture(Gdx.files.internal("snakeHead.png"));
-
-        Rectangle snakeHeadChunk = new Rectangle();
-        Rectangle snakeTailChunk = new Rectangle();
-        Rectangle snakeBodyChunk = new Rectangle();
-
-        snakeParts = new Array<Rectangle>();
-
-        snakeParts.add(snakeHeadChunk, snakeTailChunk, snakeBodyChunk);
-
-        snakeParts.get(0).x = 800/2F;
-        snakeParts.get(0).y = 480/2F;
-
-        for(int i = 2; i < snakeParts.size; i++){
-            if(i == 2){
-                snakeParts.get(i).x = snakeParts.get(0).x - 75;
-                snakeParts.get(i).y = snakeParts.get(0).y;
-            }else {
-                snakeParts.get(i).x = snakeParts.get(i-1).x - 75;
-                snakeParts.get(i).y = snakeParts.get(i-1).y;
-            }
-        }
-        snakeParts.get(1).x = snakeParts.get(snakeParts.size - 1).x - 75;
-        snakeParts.get(1).y = snakeParts.get(snakeParts.size - 1).y;
+        snake = new Snake();
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
     }
 
-    private void addNewSnakePart(){
-        Rectangle newSnakePart = new Rectangle();
-        snakeParts.add(newSnakePart);
-        snakeParts.get(snakeParts.size - 1).x = snakeParts.get(1).x;
-        snakeParts.get(snakeParts.size - 1).y = snakeParts.get(1).y;
-        snakeParts.get(1).x = snakeParts.get(snakeParts.size - 1).x - 75;
-        snakeParts.get(1).y = snakeParts.get(snakeParts.size - 1).y;
-    }
-
     private void drawEverySnakePart(){
-        for(int i = 0; i < snakeParts.size; i++){
-            if( i == 0)
-                game.batch.draw(new TextureRegion(snakeHead), changeSnakeChunkX(i), changeSnakeChunkY(i),75/2F, 45/2F, 75, 45,1,1, rotation);
+        for(int i = 0; i < snake.snakeParts.size; i++){
+            if(i == 0)
+                game.batch.draw(new TextureRegion(snake.snakeHead), changeSnakeChunkX(i), changeSnakeChunkY(i),64/2F, 64/2F, 64, 64,1,1, rotation);
             else if(i == 1)
-                game.batch.draw(new TextureRegion(snakeTail), changeSnakeChunkX(i), changeSnakeChunkY(i),75/2F, 45/2F, 75, 45,1,1, rotationShared);
+                game.batch.draw(new TextureRegion(snake.snakeTail), changeSnakeChunkX(i), changeSnakeChunkY(i),64/2F, 64/2F, 64, 64,1,1, rotation);
             else
-                game.batch.draw(new TextureRegion(snakeBody), changeSnakeChunkX(i), changeSnakeChunkY(i), 75/2F, 45/2F, 75, 45, 1, 1, rotationShared); // <-- draw only one thing per method
+                game.batch.draw(new TextureRegion(snake.snakeBody), changeSnakeChunkX(i), changeSnakeChunkY(i)); // <-- draw only one thing per method
         }
     }
 
     private float changeSnakeChunkX(int j){
         if(j == 0){
             if(isMovingRight) {
-                //lastDrawedFrameTime = TimeUtils.nanoTime();
-                return snakeParts.get(j).x += 1;
+                return snake.snakeParts.get(j).x += 1;
             }else if(isMovingLeft) {
-                //lastDrawedFrameTime = TimeUtils.nanoTime();
-                return snakeParts.get(j).x -= 1;
+                return snake.snakeParts.get(j).x -= 1;
             }else
-                return snakeParts.get(j).x;
+                return snake.snakeParts.get(j).x;
         }else{
-            if((snakeParts.get(j).x < snakeParts.get(0).x) && (snakeParts.get(j).y == snakeParts.get(0).y)) {
-                //lastDrawedFrameTime = TimeUtils.nanoTime();
-                return snakeParts.get(j).x += 1;
-            }else if((snakeParts.get(j).x < snakeParts.get(0).x) && (snakeParts.get(j).y != snakeParts.get(0).y)){
-                return snakeParts.get(j).x += 1;
-            }else if((snakeParts.get(j).x > snakeParts.get(0).x) && (snakeParts.get(j).y == snakeParts.get(0).y)) {
-                //lastDrawedFrameTime = TimeUtils.nanoTime();
-                return snakeParts.get(j).x -= 1;
-            }else if((snakeParts.get(j).x > snakeParts.get(0).x) && (snakeParts.get(j).y != snakeParts.get(0).y)) {
-                return snakeParts.get(j).x -= 1;
-            }else
-                return snakeParts.get(j).x;
+            if((snake.snakeParts.get(j).x < snake.snakeParts.get(0).x) && (snake.snakeParts.get(j).y == snake.snakeParts.get(0).y)) {
+                return snake.snakeParts.get(j).x += 1;
+            }else if((snake.snakeParts.get(j).x < snake.snakeParts.get(0).x) && (snake.snakeParts.get(j).y != snake.snakeParts.get(0).y)){
+                return snake.snakeParts.get(j).x += 1;
+            }else if((snake.snakeParts.get(j).x > snake.snakeParts.get(0).x) && (snake.snakeParts.get(j).y == snake.snakeParts.get(0).y)) {
+                return snake.snakeParts.get(j).x -= 1;
+            }else if((snake.snakeParts.get(j).x > snake.snakeParts.get(0).x) && (snake.snakeParts.get(j).y != snake.snakeParts.get(0).y)) {
+                return snake.snakeParts.get(j).x -= 1;
+            }else {
+                return snake.snakeParts.get(j).x;
+            }
         }
     }
 
     private float changeSnakeChunkY(int j){
         if(j == 0) {
             if (isMovingUp) {
-                //lastDrawedFrameTime = TimeUtils.nanoTime();
-                return snakeParts.get(j).y += 1;
+                return snake.snakeParts.get(j).y += 1;
             } else if (isMovingDown) {
-                //lastDrawedFrameTime = TimeUtils.nanoTime();
-                return snakeParts.get(j).y -= 1;
+                return snake.snakeParts.get(j).y -= 1;
             } else
-                return snakeParts.get(j).y;
+                return snake.snakeParts.get(j).y;
         }else{
-            if((snakeParts.get(j).y < snakeParts.get(0).y) && (snakeParts.get(j).x == snakeParts.get(0).x)) {
-                //lastDrawedFrameTime = TimeUtils.nanoTime();
-                return snakeParts.get(j).y += 1;
-            }else if((snakeParts.get(j).y > snakeParts.get(0).y) && (snakeParts.get(j).x == snakeParts.get(0).x)){
-                //lastDrawedFrameTime = TimeUtils.nanoTime();
-                return snakeParts.get(j).y -= 1;
+            if((snake.snakeParts.get(j).y < snake.snakeParts.get(0).y) && (snake.snakeParts.get(j).x == snake.snakeParts.get(0).x)) {
+                return snake.snakeParts.get(j).y += 1;
+            }else if((snake.snakeParts.get(j).y > snake.snakeParts.get(0).y) && (snake.snakeParts.get(j).x == snake.snakeParts.get(0).x)){
+                return snake.snakeParts.get(j).y -= 1;
             }else
-                return snakeParts.get(j).y;
+                return snake.snakeParts.get(j).y;
         }
     }
 
@@ -147,36 +97,32 @@ public class GameScreen implements Screen {
         drawEverySnakePart();
         game.batch.end();
 
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && !isMovingRight) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT) && !isMovingRight) {
             isMovingUp = false;
             isMovingRight = false;
             isMovingDown = false;
             isMovingLeft = true;
-            rotationShared = rotation;
             rotation = 180;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && !isMovingLeft){
+        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT) && !isMovingLeft){
             isMovingUp = false;
             isMovingRight = true;
             isMovingDown = false;
             isMovingLeft = false;
-            rotationShared = rotation;
             rotation = 0;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP) && !isMovingDown){
+        if (Gdx.input.isKeyJustPressed(Input.Keys.UP) && !isMovingDown){
             isMovingUp = true;
             isMovingRight = false;
             isMovingDown = false;
             isMovingLeft = false;
-            rotationShared = rotation;
             rotation = 90;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && !isMovingUp){
+        if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN) && !isMovingUp){
             isMovingUp = false;
             isMovingRight = false;
             isMovingDown = true;
             isMovingLeft = false;
-            rotationShared = rotation;
             rotation = 270;
         }
 
@@ -188,7 +134,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
-        //addNewSnakePart();
+        snake.addNewSnakePart(5);
     }
 
     @Override
@@ -205,8 +151,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-        snakeHead.dispose();
-        snakeBody.dispose();
-        snakeTail.dispose();
+        snake.snakeHead.dispose();
+        snake.snakeBody.dispose();
+        snake.snakeTail.dispose();
     }
 }
---
+++ core/src/com/mygdx/game/Snake.java
@@ -0,0 +1,54 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+
+class Snake {
+    Texture snakeBody;
+    Texture snakeTail;
+    Texture snakeHead;
+    Array<Rectangle> snakeParts;
+
+    Snake(){
+        snakeBody = new Texture(Gdx.files.internal("snakeBody.png"));
+        snakeTail = new Texture(Gdx.files.internal("snakeTail.png"));
+        snakeHead = new Texture(Gdx.files.internal("snakeHead.png"));
+
+        Rectangle snakeHeadChunk = new Rectangle();
+        Rectangle snakeTailChunk = new Rectangle();
+        Rectangle snakeBodyChunk = new Rectangle();
+
+        snakeParts = new Array<Rectangle>();
+
+        snakeParts.add(snakeHeadChunk, snakeTailChunk, snakeBodyChunk);
+
+        snakeParts.get(0).x = 800/2F;
+        snakeParts.get(0).y = 480/2F;
+
+        for(int i = 2; i < snakeParts.size; i++){
+            if(i == 2){
+                snakeParts.get(i).x = snakeParts.get(0).x - 64;
+                snakeParts.get(i).y = snakeParts.get(0).y;
+            }else {
+                snakeParts.get(i).x = snakeParts.get(i-1).x - 64;
+                snakeParts.get(i).y = snakeParts.get(i-1).y;
+            }
+        }
+        snakeParts.get(1).x = snakeParts.get(snakeParts.size - 1).x - 64;
+        snakeParts.get(1).y = snakeParts.get(snakeParts.size - 1).y;
+    }
+
+    void addNewSnakePart(int howManySnakes){
+        for(int  i = 0; i < howManySnakes; i++) {
+            Rectangle newSnakePart = new Rectangle();
+            snakeParts.add(newSnakePart);
+            snakeParts.get(snakeParts.size - 1).x = snakeParts.get(1).x;
+            snakeParts.get(snakeParts.size - 1).y = snakeParts.get(1).y;
+            snakeParts.get(1).x = snakeParts.get(snakeParts.size - 1).x - 64;
+            snakeParts.get(1).y = snakeParts.get(snakeParts.size - 1).y;
+        }
+    }
+
+}
